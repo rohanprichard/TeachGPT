2b99a1b368e9b44934212a5052593e12cdbe86f5
Rohan Richard <rohanrichard@Rohans-MacBook-Pro.local>
2024-01-31 18:43:26 +0530

    using postgresql

--- model_server/database/database.py
+++ model_server/database/database.py
@@ -15,7 +15,7 @@ SQLALCHEMY_DATABASE_URL = (cfg["DB_URL"])
 logger.info("Starting SQL DB engine")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
